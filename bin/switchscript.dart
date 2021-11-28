2cb1be4bec85829d19a16f4649af30c6e1ecd2df
Azkadev <azkadev@pop-os.localdomain>
2021-11-29 02:25:16 +0700

    update

--- bin/switchscript.dart
+++ bin/switchscript.dart
@@ -1,14 +1,4 @@
 import 'package:switchscript/switchscript.dart';
-
-void main() {
-  var tanggal = date(DateTime.now().millisecondsSinceEpoch);
-  print(tanggal.toJson());
-  print(tanggal.day);
-}
-
-<<<<<<< HEAD
 void main() async {
   print(HumanUtils.filesize(31942));
 }
-=======
->>>>>>> 94c6d4036d431d3496d1623e0a21880b29ac0db3
